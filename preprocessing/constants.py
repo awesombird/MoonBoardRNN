f023cf51be7f10c1be9dc43af79e451f7c857ecf
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2022-12-02 14:41:30 +0000

    add constants

---
+++ preprocessing/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# Project root directory
+ROOT_DIR = Path(__file__).parent.parent
+DATA_DIR = ROOT_DIR / 'raw_data'
+PREPROCESSING_DIR = ROOT_DIR / 'preprocessing'
+HOLD_FEATURES_PATH = DATA_DIR / 'HoldFeature2016.xlsx'
+HOLD_FEATURES_LEFT_HAND_PATH = DATA_DIR / 'HoldFeature2016LeftHand.csv'
+HOLD_FEATURES_RIGHT_HAND_PATH = DATA_DIR / 'HoldFeature2016RightHand.csv'
+SCRAPE_DATA_PATH = DATA_DIR / 'moonGen_scrape_2016_cp'
