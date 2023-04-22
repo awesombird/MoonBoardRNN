d615a26f7a5305958ea0bced60ba1789f1340450
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2023-04-22 16:17:24 +0100

    formatting

--- model/model_helper.py
+++ model/model_helper.py
@@ -8,56 +8,59 @@ import matplotlib.cbook as cbook
 import re
 from pathlib import Path
 
+
 def save_pickle(data, file_name):
     """
     Saves data as pickle format
     """
-    with open(file_name, 'wb') as f:
+    with open(file_name, "wb") as f:
         pickle.dump(data, f)
 
 
-def plot_confusion_matrix(Y_true, Y_predict, title = None):
+def plot_confusion_matrix(Y_true, Y_predict, title=None):
     """
     Plot the confusion matrix.
     """
-    labels = ['V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13']
+    labels = ["V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13"]
     conf_matrix = metrics.confusion_matrix(Y_true, Y_predict)
-    df_cm = pd.DataFrame((conf_matrix/np.sum(conf_matrix, axis = 1, keepdims = True)), 
-                         index = [i for i in labels],
-                         columns = [i for i in labels])
-    plt.figure(dpi = 150, figsize = (10,7))
+    df_cm = pd.DataFrame(
+        (conf_matrix / np.sum(conf_matrix, axis=1, keepdims=True)),
+        index=[i for i in labels],
+        columns=[i for i in labels],
+    )
+    plt.figure(dpi=150, figsize=(10, 7))
     sn.heatmap(df_cm, annot=True, cmap=plt.cm.Blues)
-    plt.xlabel('predicted grade')
-    plt.ylabel('actual grade')
+    plt.xlabel("predicted grade")
+    plt.ylabel("actual grade")
     if title:
         plt.title(title)
     plt.show()
-    return
+
 
 def plot_history(history, model_name):
     """
     Plot the training history of the model
     """
     # Plot training & validation accuracy values
-    acc = history.history['sparse_categorical_accuracy']
-    val_acc = history.history['val_sparse_categorical_accuracy']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+    acc = history.history["sparse_categorical_accuracy"]
+    val_acc = history.history["val_sparse_categorical_accuracy"]
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
 
-    _, axes = plt.subplots(nrows = 1, ncols = 2)
+    _, axes = plt.subplots(nrows=1, ncols=2)
     axes[0].plot(acc)
     axes[0].plot(val_acc)
-    axes[0].set_title('Accuracy of '+ model_name)
-    axes[0].set_ylabel('Accuracy')
-    axes[0].set_xlabel('Epoch')
-    axes[0].legend(['Train', 'Val'], loc='upper left')
+    axes[0].set_title("Accuracy of " + model_name)
+    axes[0].set_ylabel("Accuracy")
+    axes[0].set_xlabel("Epoch")
+    axes[0].legend(["Train", "Val"], loc="upper left")
 
     axes[1].plot(loss)
     axes[1].plot(val_loss)
-    axes[1].set_title('Loss of ' + model_name)
-    axes[1].set_ylabel('Loss')
-    axes[1].set_xlabel('Epoch')
-    axes[1].legend(['Train', 'Val'], loc='upper left')
+    axes[1].set_title("Loss of " + model_name)
+    axes[1].set_ylabel("Loss")
+    axes[1].set_xlabel("Epoch")
+    axes[1].legend(["Train", "Val"], loc="upper left")
     plt.tight_layout()
 
 
@@ -67,58 +70,73 @@ def compute_accuracy(y_true, y_predict):
     - complete_accurate: the model output match the expected output exactly
     - roughly_accurate: the model output is only different from the expected output by less than or equal to 1 grade.
     """
-    complete_accurate = np.sum(y_true == y_predict)/len(y_true)
-    roughly_accurate = np.sum(np.abs(y_true - y_predict) <= 1)/len(y_true)
+    complete_accurate = np.sum(y_true == y_predict) / len(y_true)
+    roughly_accurate = np.sum(np.abs(y_true - y_predict) <= 1) / len(y_true)
     return (complete_accurate, roughly_accurate)
 
 
-""" Draw a moonboard problem on the layout"""
-def plotAProblem(stringList, title = None, key = None):
+def plotAProblem(stringList, title=None, key=None):
+    """ Draw a moonboard problem on the layout"""
     parent = Path().cwd().parent
-    image_file = cbook.get_sample_data(parent / "raw_data" / "moonboard2016Background.jpg")
+    image_file = cbook.get_sample_data(
+        parent / "raw_data" / "moonboard2016Background.jpg"
+    )
     img = plt.imread(image_file)
     x = []
     y = []
     for hold in stringList:
-        # Using re.findall() 
-        # Splitting text and number in string  
-        res = [re.findall(r'(\w+?)(\d+)', hold.split("-")[0])[0]] 
-        
-        alphabateList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"] 
-        ixInXAxis = alphabateList.index(res[0][0]) 
-     
-        x = x + [(90 + 52 * ixInXAxis)]# * img.shape[0] / 1024]
-        y = y + [(1020 - 52 * int(res[0][1]))]# * img.shape[1] / 1024]
+        # Using re.findall()
+        # Splitting text and number in string
+        res = [re.findall(r"(\w+?)(\d+)", hold.split("-")[0])[0]]
+
+        alphabateList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+        ixInXAxis = alphabateList.index(res[0][0])
+
+        x = x + [(90 + 52 * ixInXAxis)]  # * img.shape[0] / 1024]
+        y = y + [(1020 - 52 * int(res[0][1]))]  # * img.shape[1] / 1024]
 
     # Create a figure. Equal aspect so circles look circular
-    plt.rcParams["figure.figsize"] = (10,20)
-    fig,ax = plt.subplots(1)
-    ax.set_aspect('equal')
+    plt.rcParams["figure.figsize"] = (10, 20)
+    fig, ax = plt.subplots(1)
+    ax.set_aspect("equal")
 
     # Show the image
     ax.imshow(img)
 
     # Now, loop through coord arrays, and create a circle at each x,y pair
     count = 0
-    for xx,yy in zip(x,y):
+    for xx, yy in zip(x, y):
         if yy == 84:
-            circ = plt.Circle((xx,yy), 30, color = 'r', fill=False, linewidth = 2)
+            circ = plt.Circle((xx, yy), 30, color="r", fill=False, linewidth=2)
         elif count < 2:
-            circ = plt.Circle((xx,yy), 30, color = 'g', fill=False, linewidth = 2)
+            circ = plt.Circle((xx, yy), 30, color="g", fill=False, linewidth=2)
         else:
-            circ = plt.Circle((xx,yy), 30, color = 'b', fill=False, linewidth = 2)
+            circ = plt.Circle((xx, yy), 30, color="b", fill=False, linewidth=2)
         ax.add_patch(circ)
         count = count + 1
 
     # Show the image
     if title:
         plt.title(title)
-        plt.savefig(key + '.jpg', dpi = 200)
+        plt.savefig(key + ".jpg", dpi=200)
     plt.show()
 
+
 def convert_num_to_V_grade(num):
-    dic = {0: 'V4', 1: 'V5', 2: 'V6', 3:'V7', 4:'V8', 5: 'V9', 6:'V10', 7: 'V11', 8:'V12', 9:'V13'}
+    dic = {
+        0: "V4",
+        1: "V5",
+        2: "V6",
+        3: "V7",
+        4: "V8",
+        5: "V9",
+        6: "V10",
+        7: "V11",
+        8: "V12",
+        9: "V13",
+    }
     return dic[num]
+
 
 def normalization(input_set):
     # TODO: WHAT THE FUCK ARE THESE MAGIC NUMBERS
@@ -130,22 +148,73 @@ def normalization(input_set):
     sig_hand = 2.115829552
     mu_diff = 12.118308
     sig_diff = 11.495348196
-    
-    mu_vec = np.array([mu_x, mu_y, 0, mu_hand, mu_x, mu_y, mu_hand, 0, 0, mu_x, mu_y, mu_hand, 0, 0, 0, 0, 0, 0, 0, 0, 0, mu_diff])
-    sig_vec = np.array([sig_x, sig_y, 1, sig_hand, sig_x, sig_y, sig_hand, sig_x, sig_y, sig_x, sig_y, sig_hand, sig_x, sig_y, 1, 1, 1, 1, 1, 1, 1, sig_diff])
-    
-    mask = np.zeros_like(input_set['X'])
+
+    mu_vec = np.array(
+        [
+            mu_x,
+            mu_y,
+            0,
+            mu_hand,
+            mu_x,
+            mu_y,
+            mu_hand,
+            0,
+            0,
+            mu_x,
+            mu_y,
+            mu_hand,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            mu_diff,
+        ]
+    )
+    sig_vec = np.array(
+        [
+            sig_x,
+            sig_y,
+            1,
+            sig_hand,
+            sig_x,
+            sig_y,
+            sig_hand,
+            sig_x,
+            sig_y,
+            sig_x,
+            sig_y,
+            sig_hand,
+            sig_x,
+            sig_y,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            sig_diff,
+        ]
+    )
+
+    mask = np.zeros_like(input_set["X"])
     for i in range(len(mask)):
-        mask[i, 0:int(input_set['tmax'][i]), :] = 1
-    
-    X_normalized = np.copy(input_set['X'])
+        mask[i, 0 : int(input_set["tmax"][i]), :] = 1
+
+    X_normalized = np.copy(input_set["X"])
     X_normalized -= mu_vec
     X_normalized /= sig_vec
     X_normalized *= mask
-    
+
     output_set = input_set
-    output_set['X'] = X_normalized
+    output_set["X"] = X_normalized
     return output_set
+
 
 def prepare_generated_data_for_grading(generated_problems, save_path):
     n_sample = len(generated_problems)
@@ -156,15 +225,17 @@ def prepare_generated_data_for_grading(generated_problems, save_path):
     i = 0
     for key, value in generated_problems.items():
         X_data = value.T
-        X_seq_data_merge[i, 0:X_data.shape[0], :] = X_data
+        X_seq_data_merge[i, 0 : X_data.shape[0], :] = X_data
         keys_seq_merge.append(key)
         tmax_seq_merge[i] = X_data.shape[0]
         i = i + 1
-    
-    test_set_gen = {'X': X_seq_data_merge,   
-                'keys': keys_seq_merge, 
-                'tmax': tmax_seq_merge}
-    
+
+    test_set_gen = {
+        "X": X_seq_data_merge,
+        "keys": keys_seq_merge,
+        "tmax": tmax_seq_merge,
+    }
+
     test_set_gen_normalized = normalization(test_set_gen)
     save_pickle(test_set_gen_normalized, save_path)
     return test_set_gen_normalized
